33dc5ca000fe298bd60eb869afd5fb297fde3a46
surya-palla <contactsurya2@gmail.com>
2023-11-19 21:47:12 +0530

    Added Tweet Feed and Create Tweet Functionality

--- components/FeedCard/index.tsx
+++ components/FeedCard/index.tsx
@@ -3,27 +3,33 @@ import React from "react";
 import { BiMessageRounded, BiUpload } from "react-icons/bi";
 import { FaRetweet } from "react-icons/fa6";
 import { AiOutlineHeart } from "react-icons/ai";
+import { Tweet } from "@/gql/graphql";
 
-const FeedCard: React.FC = () => {
+interface FeedCardProps {
+  data: Tweet;
+}
+
+const FeedCard: React.FC<FeedCardProps> = (props) => {
+  const { data } = props;
   return (
     <div className="border border-r-0 border-l-0 border-b-0 border-gray-600 p-5 hover:bg-slate-900 transition-all cursor-pointer">
       <div className="grid grid-cols-12 gap-3">
         <div className="col-span-1">
-          <Image
-            className="rounded-full"
-            src="https://avatars.githubusercontent.com/u/42321193?v=4"
-            alt="userImage"
-            height={50}
-            width={50}
-          />
+          {data.author?.profileImageURL && (
+            <Image
+              className="rounded-full"
+              src={data.author?.profileImageURL}
+              alt="userImage"
+              height={50}
+              width={50}
+            />
+          )}
         </div>
         <div className="col-span-11">
-          <h5>Surya Palla</h5>
-          <p>
-            The only difference between the visual skyline of Gaza city and
-            Hyderabad city tonight , is that there it’s real bombs and here it’s
-            Diwali bombs 😳
-          </p>
+          <h5>
+            {data.author?.firstName} {data.author?.lastName}
+          </h5>
+          <p>{data.content}</p>
           <div className="flex justify-between mt-5 text-xl items-center p-2 w-[90%]">
             <div>
               <BiMessageRounded />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 import {
   BsTwitter,
   BsBell,
@@ -7,7 +7,7 @@ import {
   BsBookmark,
   BsPeople,
 } from "react-icons/bs";
-import { BiSolidHome, BiSearch, BiMoney } from "react-icons/bi";
+import { BiSolidHome, BiSearch, BiMoney, BiImageAlt } from "react-icons/bi";
 import { FaRegUser } from "react-icons/fa6";
 import { CiCircleMore } from "react-icons/ci";
 import FeedCard from "@/components/FeedCard";
@@ -17,6 +17,8 @@ import { graphQLClient } from "@/clients/api";
 import { verifyUserGoogleTokenQuery } from "@/graphql/query/user";
 import { useCurrentUser } from "@/hooks/user";
 import { useQueryClient } from "@tanstack/react-query";
+import { useCreateTweet, useGetAllTweets } from "@/hooks/tweet";
+import { Tweet } from "@/gql/graphql";
 
 interface TwitterSidebarButton {
   title: string;
@@ -65,8 +67,20 @@ const sideMenubarItems: TwitterSidebarButton[] = [
 export default function Home() {
   const { user } = useCurrentUser();
 
+  const { tweets = [] } = useGetAllTweets();
+  const { mutate } = useCreateTweet();
+
   const queryClient = useQueryClient();
-  console.log(user);
+
+  const [content, setContent] = useState("");
+
+  const handleSelectImage = useCallback(() => {
+    const input = document.createElement("input");
+    input.setAttribute("type", "file");
+    input.setAttribute("accept", "image/*");
+    input.click();
+  }, []);
+  // console.log(user);
   const handleLoginWithGoogle = useCallback(
     async (cred: CredentialResponse) => {
       const googleToken = cred.credential;
@@ -87,10 +101,17 @@ export default function Home() {
         window.localStorage.setItem("__twitter_token", verifyGoogleToken);
       }
 
-      await queryClient.invalidateQueries();
+      await queryClient.invalidateQueries({ queryKey: ["curent-user"] });
     },
     [queryClient]
   );
+
+  const handleCreateTweet = useCallback(() => {
+    mutate({
+      content,
+    });
+    setContent("");
+  }, [content, mutate]);
 
   return (
     <div>
@@ -113,7 +134,7 @@ export default function Home() {
             </ul>
             <div className="mt-3 pr-10">
               <button className="bg-[#1d9bf0] py-2 px-4 text-lg font-semibold rounded-full w-full">
-                Tweet s
+                Tweet
               </button>
             </div>
           </div>
@@ -137,12 +158,47 @@ export default function Home() {
           )}
         </div>
         <div className="col-span-5 border-r-[1px] border-l-[1px] border-gray-600 h-screen overflow-scroll no-scrollbar">
-          <FeedCard />
-          <FeedCard />
-          <FeedCard />
-          <FeedCard />
-          <FeedCard />
-          <FeedCard />
+          <div>
+            <div className="border border-r-0 border-l-0 border-b-0 border-gray-600 p-5 hover:bg-slate-900 transition-all cursor-pointer">
+              <div className="grid grid-cols-12 gap-3">
+                <div className="col-span-1">
+                  {user?.profileImageURL && (
+                    <Image
+                      className="rounded-full"
+                      src={user?.profileImageURL}
+                      alt="userImage"
+                      height={50}
+                      width={50}
+                    />
+                  )}
+                </div>
+                <div className="col-span-11">
+                  <textarea
+                    className="border-b border-slate-700 w-full bg-transparent text-xl px-3 focus:outline-none"
+                    placeholder="What's happening?"
+                    rows={3}
+                    value={content}
+                    onChange={(e) => setContent(e.target.value)}
+                  ></textarea>
+                  <div className="mt-2 flex justify-between items-center">
+                    <BiImageAlt
+                      onClick={handleSelectImage}
+                      className="text-xl"
+                    />
+                    <button
+                      onClick={handleCreateTweet}
+                      className="bg-[#1d9bf0] py-2 px-4 text-sm font-semibold rounded-full"
+                    >
+                      Tweet
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {tweets?.map((tweet) =>
+            tweet ? <FeedCard key={tweet?.id} data={tweet as Tweet} /> : null
+          )}
         </div>
         <div className="col-span-3 p-5">
           {!user && (
